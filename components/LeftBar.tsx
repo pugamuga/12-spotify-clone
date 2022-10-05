2352eceeb0b16db6319ce84546181b7e7e33deb4
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-10-05 12:25:17 +0300

    fix errors

--- components/LeftBar.tsx
+++ components/LeftBar.tsx
@@ -1,12 +1,26 @@
 import Image from "next/image";
+import {HomeIcon, ChartBarIcon, ClockIcon } from "@heroicons/react/24/solid"
+import {FaMicrophoneAlt} from "react-icons/fa"
+import {RiCompassFill} from "react-icons/ri"
+import {BsThreeDots} from "react-icons/bs"
 
-export default function LeftBar():JSX.Element {
+export default function LeftBar(): JSX.Element {
   return (
     <section className=" fixed z-40 top-0 left-0 flex flex-col p-4 items-center bg-black w-[90px] h-screen space-y-8">
-      <Image src={`https://rb.gy/xkacau`} height={56} width={56} objectFit="contain"/>
+      <Image
+        src={`https://rb.gy/xkacau`}
+        height={56}
+        width={56}
+        objectFit="contain"
+      />
       <div>
-
+        <HomeIcon className="leftBarIcon text-white opacity-[85%]"/>
+        <RiCompassFill className="leftBarIcon"/>
+        <FaMicrophoneAlt className="leftBarIcon"/>
+        <ChartBarIcon className="leftBarIcon"/>
+        <ClockIcon className="leftBarIcon"/>
+        <BsThreeDots className="leftBarIcon"/>
       </div>
     </section>
-  )
+  );
 }
